2fb2c8b55a5d0c48a0b32b1e76ac21d838d1d365
Claudia El Khatib <claudiacv22@gmail.com>
2017-08-28 11:53:31 -0400

    new UI look and structure

---
+++ assets/app.js
@@ -0,0 +1,6 @@
+'use strict'
+
+module.exports = {
+// host: 'https://immense-sands-36854.herokuapp.com'
+  host: 'http://localhost:4741'
+}
--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 const config = {
   apiOrigins: {
-    production: 'https://ga-wdi-boston.herokuapp.com'
+    production: 'https://immense-sands-36854.herokuapp.com'
   }
 }
 
